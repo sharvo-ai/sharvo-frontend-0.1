147134555ecc4340119fb558716bdc51eff24f1f
sharvo-ai <tanmaytawari123@gmail.com>
2025-06-26 12:33:20 +0530

    Create main.jsx

---
+++ public/src/main.jsx
@@ -0,0 +1,9 @@
+01 import React from 'react'
+02 import ReactDOM from 'react-dom/client'
+03 import App from './App.jsx'
+04 
+05 ReactDOM.createRoot(document.getElementById('root')).render(
+06   <React.StrictMode>
+07     <App />
+08   </React.StrictMode>
+09 )
